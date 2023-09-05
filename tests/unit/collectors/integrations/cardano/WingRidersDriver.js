944777e4b7b25d29c00c78b6b74d67da0fd3e972
WCodeUpdate <gabriela.guerragalan+webby@gmail.com>
2023-09-05 14:49:07 +0300

    feat(collecctors): use adress data instead of utxos in WingRidersDriver

--- tests/unit/collectors/integrations/cardano/WingRidersDriver.js
+++ tests/unit/collectors/integrations/cardano/WingRidersDriver.js
@@ -20,18 +20,16 @@ const pair = {
 };
 
 const lpData = {
-    utxos: [
-        {
-            amount: [
-                {
-                    unit: 'c0ee29a85b13209423b10447d3c2e6a50641a15c57770e27cb9d507357696e67526964657273',
-                    quantity: '1',
-                },
-                { unit: 'lovelace', quantity: '10' },
-                { unit: 'test', quantity: '1' },
-            ],
-        },
-    ],
+    assets: {
+        amount: [
+            {
+                unit: 'c0ee29a85b13209423b10447d3c2e6a50641a15c57770e27cb9d507357696e67526964657273',
+                quantity: '1',
+            },
+            { unit: 'lovelace', quantity: '10' },
+        ],
+    },
+
     reserveA: '1',
     reserveB: '10',
 };
@@ -41,7 +39,7 @@ const pairQuote = '10';
 test.beforeEach((t) => {
     sandbox = sinon.createSandbox();
 
-    t.context.minswapDriver = new cardanoDrivers['wing_riders']({
+    t.context.wingRidersDriver = new cardanoDrivers['wing_riders']({
         projectId: 'test',
     });
 });
@@ -51,14 +49,16 @@ test.afterEach(() => {
 });
 
 test('the "getExchangeRate" method should get storage and pair price.', async (t) => {
-    const { minswapDriver } = t.context;
+    const { wingRidersDriver } = t.context;
 
-    const getReservesStub = sinon.stub(minswapDriver, 'getReserves').returns({
-        poolASize: new BigNumber(lpData.reserveA),
-        poolBSize: new BigNumber(lpData.reserveB),
-    });
+    const getReservesStub = sinon
+        .stub(wingRidersDriver, 'getReserves')
+        .returns({
+            poolASize: new BigNumber(lpData.reserveA),
+            poolBSize: new BigNumber(lpData.reserveB),
+        });
 
-    const result = await minswapDriver.getExchangeRate(pair);
+    const result = await wingRidersDriver.getExchangeRate(pair);
 
     sinon.assert.calledOnce(getReservesStub);
     t.deepEqual(result, {
@@ -69,15 +69,15 @@ test('the "getExchangeRate" method should get storage and pair price.', async (t
 });
 
 test('the "getReserves" method should return pool sizes.', async (t) => {
-    const { minswapDriver } = t.context;
+    const { wingRidersDriver } = t.context;
 
-    const addressesUtxosStub = sinon
-        .stub(minswapDriver.blockFrost, 'addressesUtxos')
-        .returns(lpData.utxos);
+    const addressesStub = sinon
+        .stub(wingRidersDriver.blockFrost, 'addresses')
+        .returns(lpData.assets);
 
-    const result = await minswapDriver.getReserves(pair);
+    const result = await wingRidersDriver.getReserves(pair);
 
-    sinon.assert.calledOnce(addressesUtxosStub);
+    sinon.assert.calledOnce(addressesStub);
     t.deepEqual(
         {
             poolASize: result.poolASize.toString(),
