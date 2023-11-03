2aece787f25545070ede1e05ef0b367d21698a42
WCodeUpdate <gabriela.guerragalan+webby@gmail.com>
2023-11-03 13:18:42 +0200

    fix(tests): CollectorScheduler expects logger

--- ava.config.cjs
+++ ava.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     nodeArguments: ['--experimental-modules', '--experimental-json-modules'],
     serial: true,
     verbose: false,
-    files: ['tests/e2e/index.test.js', 'tests/unit/**/**.js'],
+    files: ['tests/unit/**/**.js'],
     concurrency: 1,
     environmentVariables: {
         NODE_ENV: 'test',
