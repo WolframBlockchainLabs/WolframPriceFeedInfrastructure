471c000b374e2a513478c191ff9123000caa5a5f
WCodeUpdate <gabriela.guerragalan+webby@gmail.com>
2023-11-03 13:30:16 +0200

    fix(tests): Database Writer tests fix

--- ava.config.cjs
+++ ava.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     nodeArguments: ['--experimental-modules', '--experimental-json-modules'],
     serial: true,
     verbose: false,
-    files: ['tests/unit/**/**.js'],
+    files: ['tests/e2e/index.test.js', 'tests/unit/**/**.js'],
     concurrency: 1,
     environmentVariables: {
         NODE_ENV: 'test',
