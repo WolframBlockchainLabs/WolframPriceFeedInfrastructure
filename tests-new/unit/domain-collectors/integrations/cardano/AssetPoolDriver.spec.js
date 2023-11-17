db6a9b2e5ccf324de893721c25ce77fbca7c3b61
WCodeUpdate <gabriela.guerragalan+webby@gmail.com>
2023-11-17 13:06:34 +0200

    feat(new-tests): add unit tests for managers

---
+++ tests-new/unit/domain-collectors/integrations/cardano/AssetPoolDriver.spec.js
@@ -0,0 +1,82 @@
+import cardanoDrivers from '../../../../../lib/domain-collectors/integrations/cardano/index.js';
+import BigNumber from 'bignumber.js';
+
+describe('[domain-collectors/integrations/cardano]: AssetPoolDriver Tests Suite', () => {
+    const context = {};
+
+    const pair = {
+        pool: 'addr1z8nvjzjeydcn4atcd93aac8allvrpjn7pjr2qsweukpnaytvcg6zm2vds6mz9x3h3yalqmnf24w86m09n40q3tgqxjms9yu6v8',
+        in: {
+            address:
+                'c0ee29a85b13209423b10447d3c2e6a50641a15c57770e27cb9d507357696e67526964657273',
+            decimals: 0,
+        },
+        out: {
+            address: 'lovelace',
+            decimals: 0,
+        },
+    };
+
+    const lpData = {
+        assets: {
+            amount: [
+                {
+                    unit: 'c0ee29a85b13209423b10447d3c2e6a50641a15c57770e27cb9d507357696e67526964657273',
+                    quantity: '1',
+                },
+                { unit: 'lovelace', quantity: '10' },
+            ],
+        },
+
+        reserveA: '1',
+        reserveB: '10',
+    };
+
+    const pairQuote = '10';
+
+    beforeEach(() => {
+        context.wingRidersDriver = new cardanoDrivers['wing_riders']({
+            projectId: 'test',
+        });
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    test('the "getExchangeRate" method should get storage and pair price', async () => {
+        jest.spyOn(context.wingRidersDriver, 'getReserves').mockResolvedValue({
+            poolASize: new BigNumber(lpData.reserveA),
+            poolBSize: new BigNumber(lpData.reserveB),
+        });
+
+        const result = await context.wingRidersDriver.getExchangeRate(pair);
+
+        expect(context.wingRidersDriver.getReserves).toHaveBeenCalledTimes(1);
+        expect(result).toEqual({
+            exchangeRate: pairQuote,
+            poolASize: lpData.reserveA,
+            poolBSize: lpData.reserveB,
+        });
+    });
+
+    test('the "getReserves" method should return pool sizes', async () => {
+        jest.spyOn(
+            context.wingRidersDriver.blockFrost,
+            'addresses',
+        ).mockResolvedValue(lpData.assets);
+
+        const result = await context.wingRidersDriver.getReserves(pair);
+
+        expect(
+            context.wingRidersDriver.blockFrost.addresses,
+        ).toHaveBeenCalledTimes(1);
+        expect({
+            poolASize: result.poolASize.toString(),
+            poolBSize: result.poolBSize.toString(),
+        }).toEqual({
+            poolASize: lpData.reserveA,
+            poolBSize: lpData.reserveB,
+        });
+    });
+});
---
+++ tests-new/unit/domain-collectors/integrations/cardano/BaseCardanoDriver.spec.js
@@ -0,0 +1,54 @@
+// eslint-disable-next-line import/no-unresolved
+import BigNumber from 'bignumber.js';
+import BaseCardanoDriver from '../../../../../lib/domain-collectors/integrations/cardano/BaseCardanoDriver.js';
+
+describe('[domain-collectors/integrations/cardano]: BaseCardanoDriver Tests Suite', () => {
+    const context = {};
+
+    const pair = {
+        pool: '6aa2153e1ae896a95539c9d62f76cedcdabdcdf144e564b8955f609d660cf6a2',
+        in: {
+            address: 'lovelace',
+            decimals: 0,
+        },
+        out: {
+            address:
+                '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c64d494e',
+            decimals: 0,
+        },
+    };
+
+    const lpData = {
+        reserveA: '1',
+        reserveB: '10',
+        assetA: 'lovelace',
+    };
+
+    const pairQuote = '10';
+
+    beforeEach(() => {
+        context.minswapDriver = new BaseCardanoDriver({
+            projectId: 'test',
+        });
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    test('the "getExchangeRate" method should get storage and pair price', async () => {
+        jest.spyOn(context.minswapDriver, 'getReserves').mockReturnValue({
+            poolASize: new BigNumber(lpData.reserveA),
+            poolBSize: new BigNumber(lpData.reserveB),
+        });
+
+        const result = await context.minswapDriver.getExchangeRate(pair);
+
+        expect(context.minswapDriver.getReserves).toHaveBeenCalledTimes(1);
+        expect(result).toEqual({
+            exchangeRate: pairQuote,
+            poolASize: lpData.reserveA,
+            poolBSize: lpData.reserveB,
+        });
+    });
+});
