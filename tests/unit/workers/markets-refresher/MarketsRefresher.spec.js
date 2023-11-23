9a41e3f650eb5e6403477c16cc4c410de7dabb48
WCodeUpdate <gabriela.guerragalan+webby@gmail.com>
2023-11-23 19:03:39 +0200

    feat(workers): MarketRefresher rework

--- tests/unit/workers/markets-refresher/MarketsRefresher.spec.js
+++ tests/unit/workers/markets-refresher/MarketsRefresher.spec.js
@@ -32,6 +32,7 @@ describe('[markets-refresher]: MarketsRefresher Tests Suite', () => {
         context.loggerStub = {
             info: jest.fn(),
             warning: jest.fn(),
+            error: jest.fn(),
         };
 
         context.marketsRefresher = new MarketsRefresher({
@@ -44,68 +45,105 @@ describe('[markets-refresher]: MarketsRefresher Tests Suite', () => {
         jest.restoreAllMocks();
     });
 
-    test('the execute method passes symbols to the refreshMarkets method if exchange was found', async () => {
-        const refreshMarketsSpy = jest
-            .spyOn(context.marketsRefresher, 'refreshMarkets')
-            .mockImplementation(() => Promise.resolve());
+    test('execute method handles errors in refreshExchange', async () => {
+        const error = new Error('Exchange refresh failed');
+        jest.spyOn(
+            context.marketsRefresher,
+            'refreshExchange',
+        ).mockRejectedValue(error);
 
         await context.marketsRefresher.execute([
-            {
-                id: 'binance',
-                name: 'Binance',
-                symbols: ['BTC/EUR'],
-            },
+            { id: 'binance', name: 'Binance', symbols: ['BTC/EUR'] },
         ]);
 
-        expect(context.ExchangeStub.findOne).toHaveBeenCalledTimes(1);
-        expect(refreshMarketsSpy).toHaveBeenCalledTimes(1);
-    });
-
-    test('the execute method logs a warning if exchange was not found', async () => {
-        context.ExchangeStub.findOne.mockResolvedValue(null);
-        const refreshMarketsSpy = jest
-            .spyOn(context.marketsRefresher, 'refreshMarkets')
-            .mockImplementation(() => Promise.resolve());
-
-        await context.marketsRefresher.execute([
-            {
-                id: 'binance',
-                name: 'Binance',
-                symbols: ['BTC/EUR'],
-            },
-        ]);
-
-        expect(context.ExchangeStub.findOne).toHaveBeenCalledTimes(1);
-        expect(context.loggerStub.warning).toHaveBeenCalledTimes(1);
-        expect(refreshMarketsSpy).not.toHaveBeenCalled();
-    });
-
-    test('the refreshMarkets method tries to update a market if it was found', async () => {
-        await context.marketsRefresher.refreshMarkets(
-            {
-                externalExchangeId: 'binance',
-                name: 'Binance',
-            },
-            ['BTC/EUR'],
+        expect(context.loggerStub.error).toHaveBeenCalledWith(
+            expect.stringContaining(
+                `Error refreshing [Binance] exchange: ${error.message}`,
+            ),
         );
+    });
+
+    test('refreshExchange method processes exchange and calls refreshMarkets', async () => {
+        await context.marketsRefresher.refreshExchange({
+            id: 'binance',
+            name: 'Binance',
+            symbols: ['BTC/EUR'],
+        });
+
+        expect(context.ExchangeStub.findOne).toHaveBeenCalledWith(
+            expect.any(Object),
+        );
+        expect(context.loggerStub.info).toHaveBeenCalledWith(
+            expect.stringContaining(`Refreshing [Binance] exchange`),
+        );
+    });
+
+    test('refreshMarkets method processes multiple symbols', async () => {
+        const exchange = { externalExchangeId: 'binance', name: 'Binance' };
+
+        await context.marketsRefresher.refreshMarkets(exchange, [
+            'BTC/EUR',
+            'ETH/USD',
+        ]);
 
         expect(context.MarketStub.findOne).toHaveBeenCalledTimes(1);
         expect(context.marketInstanceStub.update).toHaveBeenCalledTimes(1);
     });
 
-    test('the refreshMarkets method logs a warning if market was not found', async () => {
+    test('updateMarket method logs warning if market not found', async () => {
         context.MarketStub.findOne.mockResolvedValue(null);
 
-        await context.marketsRefresher.refreshMarkets(
-            {
-                externalExchangeId: 'binance',
-                name: 'Binance',
-            },
-            ['BTC/EUR'],
+        await context.marketsRefresher.updateMarket(
+            { name: 'Binance' },
+            { 'BTC/EUR': {} },
+            'BTC/EUR',
         );
 
-        expect(context.MarketStub.findOne).toHaveBeenCalledTimes(1);
-        expect(context.loggerStub.warning).toHaveBeenCalledTimes(1);
-        expect(context.marketInstanceStub.update).not.toHaveBeenCalled();
+        expect(context.loggerStub.warning).toHaveBeenCalledWith(
+            expect.stringContaining(
+                `Could not find Market for [Binance & BTC/EUR]`,
+            ),
+        );
+    });
+
+    test('updateMarket method handles errors in market update', async () => {
+        const error = new Error('Market update failed');
+        context.marketInstanceStub.update.mockRejectedValue(error);
+
+        await context.marketsRefresher.updateMarket(
+            { name: 'Binance' },
+            { 'BTC/EUR': {} },
+            'BTC/EUR',
+        );
+
+        expect(context.loggerStub.error).toHaveBeenCalledWith(
+            expect.stringContaining(
+                `Error updating market [Binance & BTC/EUR]: ${error.message}`,
+            ),
+        );
+    });
+
+    test('refreshExchange method logs warning if exchange is not found', async () => {
+        context.ExchangeStub.findOne.mockResolvedValue(null);
+
+        const exchangeConfig = {
+            id: 'binance',
+            name: 'Binance',
+            symbols: ['BTC/EUR'],
+        };
+
+        await context.marketsRefresher.refreshExchange(exchangeConfig);
+
+        expect(context.ExchangeStub.findOne).toHaveBeenCalledWith({
+            where: {
+                externalExchangeId: exchangeConfig.id,
+                name: exchangeConfig.name,
+            },
+        });
+        expect(context.loggerStub.warning).toHaveBeenCalledWith(
+            expect.stringContaining(
+                `Could not find ${exchangeConfig.name} exchange`,
+            ),
+        );
     });
 });
