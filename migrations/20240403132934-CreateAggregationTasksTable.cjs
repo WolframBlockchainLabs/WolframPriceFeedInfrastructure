d4092ea55ca96d4271919bc931e463154c00eb19
WCodeUpdate <gabriela.guerragalan+webby@gmail.com>
2024-04-03 16:40:38 +0300

    feat(domain-model): aggregation results and tasks models

---
+++ migrations/20240403132934-CreateAggregationTasksTable.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('AggregationTasks', {
+            id: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            type: {
+                type: Sequelize.ENUM(
+                    'CANDLES_DISCRETE_AGGREGATION',
+                    'CANDLES_COMPLETE_AGGREGATION',
+                ),
+                allowNull: false,
+            },
+            status: {
+                type: Sequelize.ENUM(
+                    'PENDING',
+                    'PROCESSING',
+                    'COMPLETED',
+                    'ERROR',
+                ),
+                allowNull: false,
+                defaultValue: 'PENDING',
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: async (queryInterface) => {
+        await queryInterface.dropTable('AggregationTasks');
+    },
+};
---
+++ migrations/20240403133154-CreateDiscreteAggregationResultsTable.cjs
@@ -0,0 +1,76 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('DiscreteAggregationResults', {
+            id: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            symbol: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            rangeDateStart: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            rangeDateEnd: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            timeframeMinutes: {
+                type: Sequelize.SMALLINT,
+                allowNull: false,
+            },
+            results: {
+                type: Sequelize.JSON,
+                allowNull: false,
+            },
+            taskId: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+                references: {
+                    model: 'AggregationTasks',
+                    key: 'id',
+                },
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+
+        await queryInterface.addIndex(
+            'DiscreteAggregationResults',
+            ['rangeDateStart', 'rangeDateEnd', 'symbol'],
+            {
+                name: 'DiscreteAggregationResults_range_search_idx',
+            },
+        );
+
+        await queryInterface.addIndex(
+            'DiscreteAggregationResults',
+            ['symbol'],
+            {
+                name: 'DiscreteAggregationResults_symbol_key',
+            },
+        );
+    },
+
+    down: async (queryInterface) => {
+        await queryInterface.removeIndex(
+            'DiscreteAggregationResults',
+            'DiscreteAggregationResults_range_search_idx',
+        );
+        await queryInterface.removeIndex(
+            'DiscreteAggregationResults',
+            'DiscreteAggregationResults_symbol_key',
+        );
+
+        await queryInterface.dropTable('DiscreteAggregationResults');
+    },
+};
