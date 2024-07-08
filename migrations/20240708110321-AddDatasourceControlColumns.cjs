9188e8a51d9226a716c77fe344fe74979a41b60d
WCodeUpdate <gabriela.guerragalan+webby@gmail.com>
2024-07-08 17:17:17 +0300

    fix(migrations): data source handling in exchanges

--- migrations/20240708110321-AddDatasourceControlColumns.cjs
+++ migrations/20240708110321-AddDatasourceControlColumns.cjs
@@ -3,6 +3,7 @@ module.exports = {
         await queryInterface.addColumn('Exchanges', 'dataSource', {
             type: Sequelize.STRING,
             allowNull: false,
+            defaultValue: 'default_source',
         });
 
         await queryInterface.addColumn('Markets', 'meta', {
@@ -23,7 +24,9 @@ module.exports = {
             defaultValue: false,
         });
 
-        await queryInterface.changeColumn('Markets', 'active', {
+        await queryInterface.removeColumn('Markets', 'active');
+
+        await queryInterface.addColumn('Markets', 'active', {
             type: Sequelize.BOOLEAN,
             allowNull: false,
             defaultValue: false,
@@ -31,16 +34,18 @@ module.exports = {
     },
 
     down: async (queryInterface, Sequelize) => {
-        await queryInterface.removeColumn('Exchanges', 'dataSource');
+        await queryInterface.removeColumn('Markets', 'active');
 
-        await queryInterface.removeColumn('Markets', 'meta');
-        await queryInterface.removeColumn('Markets', 'baseMeta');
-        await queryInterface.removeColumn('Markets', 'quoteMeta');
-        await queryInterface.removeColumn('Markets', 'historical');
-
-        await queryInterface.changeColumn('Markets', 'active', {
+        await queryInterface.addColumn('Markets', 'active', {
             type: Sequelize.BOOLEAN,
             allowNull: true,
         });
+
+        await queryInterface.removeColumn('Markets', 'historical');
+        await queryInterface.removeColumn('Markets', 'quoteMeta');
+        await queryInterface.removeColumn('Markets', 'baseMeta');
+        await queryInterface.removeColumn('Markets', 'meta');
+
+        await queryInterface.removeColumn('Exchanges', 'dataSource');
     },
 };
